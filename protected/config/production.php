1845c24e93665c34148f0706536b0e32ffc39428
laptop rv409 <dungdeveloper@gmail.com>
2012-05-04 00:10:04 +0700

    update settings

--- protected/config/production.php
+++ protected/config/production.php
@@ -4,42 +4,39 @@
 $main = include_once( 'main.php' );
 
 //require_once( '' );
-
 // Production configurations
 $production = array(
-	'components' => array(
-		'db' =>  array(
-                        'class' => 'CDbConnection',
-                        'connectionString' => 'mysql:host=localhost;dbname=yiiframework-cms',
-                        'username' => 'root',
-                        'password' => '',
-                        'charset' => 'UTF8',
-                        'tablePrefix' => '',
-                        'emulatePrepare' => true,
-                        'enableProfiling' => true,
-                        'schemaCacheID' => 'cache',
-                        'schemaCachingDuration' => 3600
+    'components' => array(
+        'db' => array(
+            'class' => 'CDbConnection',
+            'connectionString' => 'mysql:host=localhost;dbname=yiiframework-cms',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'UTF8',
+            'tablePrefix' => '',
+            'emulatePrepare' => true,
+            'enableProfiling' => true,
+            'schemaCacheID' => 'cache',
+            'schemaCachingDuration' => 3600
+        ),
+        'messages' => array(
+            //'onMissingTranslation' => array('MissingMessages', 'load'),
+            'cachingDuration' => 3600,
+        ),
+        'log' => array(
+            'class' => 'CLogRouter',
+            'routes' => array(
+                // Configures Yii to email all errors and warnings to an email address
+                array(
+                    'class' => 'LogEmailMessages',
+                    'levels' => 'error, warning',
+                    'emails' => 'dungdeveloper@gmail.com',
+                    'sentFrom' => 'YiiFramework CMS',
+                    'subject' => 'Application Error',
                 ),
-		'messages' => array(
-							//'onMissingTranslation' => array('MissingMessages', 'load'),
-			                'cachingDuration' => 3600,
-	         ),		
-		'log' => array(
-                        'class' => 'CLogRouter',
-			'routes' => array(
-				// Configures Yii to email all errors and warnings to an email address
-				array(
-					'class' => 'LogEmailMessages',
-					'levels' => 'error, warning',
-					'emails' => 'dungdeveloper@gmail.com',
-					'sentFrom' => 'YiiFramework CMS',
-					'subject' => 'Application Error',
-				),
-		),
-
-
             ),
         ),
+    ),
 );
 //merge both configurations and return them
 return CMap::mergeArray($main, $production);
